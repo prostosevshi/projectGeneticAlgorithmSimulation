8e4948f1c16451925adf609085cfe8cb5e2da22b
pepela <pfuntusha@gmail.com>
2025-04-10 22:54:13 +0400

    cleaner gen logic

---
+++ src/main/java/com/example/controller/GeneType.java
@@ -0,0 +1,13 @@
+package com.example.controller;
+
+public enum GeneType {
+    MOVE,
+    INTERACT,
+    UNKNOWN;
+
+    public static GeneType fromGene(int gene) {
+        if (gene >= 0 && gene <= 7) return MOVE;
+        if (gene >= 8 && gene <= 15) return INTERACT;
+        return UNKNOWN;
+    }
+}
